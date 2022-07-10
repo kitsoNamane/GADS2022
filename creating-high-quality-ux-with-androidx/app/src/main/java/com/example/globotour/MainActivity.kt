94818f0c4d44a3c160aeee4cc1796badabe0ecbd
kitsoNamane <kitsogideonnamane@gmail.com>
2022-07-10 02:13:05 +0200

    feat(globo tour): using navigation drawers

--- creating-high-quality-ux-with-androidx/app/src/main/java/com/example/globotour/MainActivity.kt
+++ creating-high-quality-ux-with-androidx/app/src/main/java/com/example/globotour/MainActivity.kt
@@ -1,14 +1,18 @@
 package com.example.globotour
 
 import android.os.Bundle
+import android.util.Log
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.onNavDestinationSelected
 import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
 import com.example.globotour.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var appBarConfiguration: AppBarConfiguration
     private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,9 +23,18 @@ class MainActivity : AppCompatActivity() {
 
         setSupportActionBar(binding.materialToolbar)
 
-        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_frag) as NavHostFragment
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_frag) as NavHostFragment
         val navController = navHostFragment.navController
-        appBarConfiguration = AppBarConfiguration(navController.graph, binding.drawerLayout)
-        setupActionBarWithNavController(navController, appBarConfiguration)
+        val appBarConfiguration = AppBarConfiguration(navController.graph, binding.drawerLayout)
+
+        binding.materialToolbar.setupWithNavController(navController, appBarConfiguration)
+        binding.navView.setupWithNavController(navController)
     }
+
+    override fun onBackPressed() {
+        if (binding.drawerLayout.isOpen) return binding.drawerLayout.close()
+        super.onBackPressed()
+    }
+
 }
